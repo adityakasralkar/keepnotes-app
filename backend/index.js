39142d10684901dba823a052dcb1b1fae5bee7ab
adityakasralkar <adityakasralkar@gmail.com>
2022-11-17 16:47:26 +0530

    Initialized Frontend & backend

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import notes from "./data/notes.js";
+
+const app = express();
+dotenv.config();
+
+
+
+const PORT  = process.env.PORT || 5000;
+
+
+app.get('/' , (req,res) => {
+    res.send("API is Runing");
+})
+
+app.get('/api/notes' , (req, res) => {
+    res.json(notes)
+}) ;
+
+app.get('/api/notes/:id' , (req, res) => {
+   const note =notes.find((n) => n._id === req.params.id);
+   
+   res.send(note);
+}) 
+
+
+app.listen(PORT , () => {
+    console.log(`Server is Runnning on Port : ${PORT}`);
+});
